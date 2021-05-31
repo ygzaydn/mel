850c4335730fb0539a24f1af83466899ad333787
cinar <ygzaydns@gmail.com>
2021-05-31 11:55:17 +0300

    v3

--- Code/src/pages/adminpage/adminpage.jsx
+++ Code/src/pages/adminpage/adminpage.jsx
@@ -8,108 +8,126 @@ import { withWindowConsumer } from "../../contexts/window/consumer";
 import { withFirebaseConsumer } from "../../firebase/consumer";
 
 const useStyles = {
-  homepageContainer: {
-    minHeight: "60vh",
-    alignItems: "flex-start",
-  },
-  loginForm: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  messageGrid: {
-    display: "flex",
-    border: "2px solid lightgray",
-    margin: "2vh",
-  },
+    homepageContainer: {
+        minHeight: "60vh",
+        alignItems: "flex-start",
+    },
+    loginForm: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    messageGrid: {
+        display: "flex",
+        border: "2px solid lightgray",
+        margin: "2vh",
+    },
 };
 
 const Adminpage = ({ classes, limit, width, firebase }) => {
-  const [login, setLogin] = useState(false);
-  const [messages, setMessages] = useState(null);
+    const [login, setLogin] = useState(false);
+    const [messages, setMessages] = useState(null);
 
-  const initialState = {
-    id: "",
-    password: "",
-  };
-  const [user, setUser] = useState(initialState);
+    const initialState = {
+        id: "",
+        password: "",
+    };
+    const [user, setUser] = useState(initialState);
 
-  const addInfo = (infoType) => (event) =>
-    setUser({ ...user, [infoType]: event.target.value });
+    const addInfo = (infoType) => (event) =>
+        setUser({ ...user, [infoType]: event.target.value });
 
-  const loginAttempt = (id, password) => {
-    if (id === "admin" && password === "admin") setLogin(true);
-  };
+    const loginAttempt = (id, password) => {
+        if (id === "admin" && password === "admin") setLogin(true);
+    };
 
-  useEffect(() => {
-    if (!messages) firebase.doGetInfoFromDb(setMessages);
-  }, []);
+    useEffect(() => {
+        if (!messages) firebase.doGetInfoFromDb(setMessages);
+    }, []);
 
-  console.log(messages);
-  return (
-    <Grid container justify="center" className={classes.homepageContainer}>
-      {!login ? (
-        <Grid item xs={12} className={classes.loginForm}>
-          <TextField
-            id="outlined-basic"
-            label="İsim"
-            multiline
-            required
-            value={user.name}
-            InputLabelProps={{
-              style: { color: "#845EC2" },
-            }}
-            onChange={addInfo("id")}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Şifre"
-            multiline
-            required
-            type="password"
-            value={user.password}
-            InputLabelProps={{
-              style: { color: "#845EC2" },
-            }}
-            onChange={addInfo("password")}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => loginAttempt(user.id, user.password)}
-          >
-            Giriş
-          </Button>
+    const deleteId = (uuid) => {
+        firebase.doRemoveComment(uuid);
+        firebase.doGetInfoFromDb(setMessages);
+    };
+
+    return (
+        <Grid container justify="center" className={classes.homepageContainer}>
+            {!login ? (
+                <Grid item xs={12} className={classes.loginForm}>
+                    <TextField
+                        id="outlined-basic"
+                        label="İsim"
+                        multiline
+                        required
+                        value={user.name}
+                        InputLabelProps={{
+                            style: { color: "#845EC2" },
+                        }}
+                        onChange={addInfo("id")}
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Şifre"
+                        multiline
+                        required
+                        type="password"
+                        value={user.password}
+                        InputLabelProps={{
+                            style: { color: "#845EC2" },
+                        }}
+                        onChange={addInfo("password")}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => loginAttempt(user.id, user.password)}
+                    >
+                        Giriş
+                    </Button>
+                </Grid>
+            ) : (
+                <Grid container>
+                    {messages.map((el) => (
+                        <Grid
+                            item
+                            xs={12}
+                            key={el}
+                            style={{ maxHeight: "10vh" }}
+                        >
+                            <Grid item xs={12} className={classes.messageGrid}>
+                                <Typography color="primary" variant="h6">
+                                    İsim: &nbsp; &nbsp; {el.name} ||
+                                </Typography>
+                                <Typography color="primary" variant="h6">
+                                    Email: &nbsp; &nbsp; {el.email} ||
+                                </Typography>
+                                <Typography color="primary" variant="h6">
+                                    Konu: &nbsp; &nbsp; {el.topic} ||
+                                </Typography>
+                                <Typography color="primary" variant="h6">
+                                    Mesaj: &nbsp; &nbsp; {el.message} ||
+                                </Typography>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => {
+                                        deleteId(el.uuid);
+                                    }}
+                                >
+                                    Sil
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
         </Grid>
-      ) : (
-        <Grid container>
-          {messages.map((el) => (
-            <Grid item xs={12} key={el} style={{ maxHeight: "10vh" }}>
-              <Grid item xs={12} className={classes.messageGrid}>
-                <Typography color="primary" variant="h6">
-                  İsim: &nbsp; &nbsp; {el.name} ||
-                </Typography>
-                <Typography color="primary" variant="h6">
-                  Email: &nbsp; &nbsp; {el.email} ||
-                </Typography>
-                <Typography color="primary" variant="h6">
-                  Konu: &nbsp; &nbsp; {el.topic} ||
-                </Typography>
-                <Typography color="primary" variant="h6">
-                  Mesaj: &nbsp; &nbsp; {el.message} ||
-                </Typography>
-              </Grid>
-            </Grid>
-          ))}
-        </Grid>
-      )}
-    </Grid>
-  );
+    );
 };
 
 export default compose(
-  withFirebaseConsumer,
-  withWindowConsumer,
-  withStyles(useStyles)
+    withFirebaseConsumer,
+    withWindowConsumer,
+    withStyles(useStyles)
 )(Adminpage);
